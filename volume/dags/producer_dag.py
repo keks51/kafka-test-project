62964ce23a9c988283400e65a14572a87e605f9c
Aleksei_Gomziakov <keks.mur@gmail.com>
2022-06-02 17:02:28 +0300

    first commit

--- volume/dags/producer_dag.py
+++ volume/dags/producer_dag.py
@@ -1,19 +1,11 @@
-import os
-import sys
 from datetime import datetime, timedelta
-from textwrap import dedent
 
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 
-
-# Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 
 with DAG(
         'kafka-producer',
-        # These args will get passed on to each operator
-        # You can override them on a per-task basis during operator initialization
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -29,17 +21,10 @@ with DAG(
         tags=['example-spark'],
         is_paused_upon_creation=False
 ) as dag:
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
     t1 = BashOperator(
         task_id='print_date',
         bash_command='date',
     )
-
-    #
-    # dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG
-    # dag.doc_md = """
-    # This is a documentation placed anywhere
-    # """  # otherwise, type it like this
 
     t2 = BashOperator(
         task_id='java',
